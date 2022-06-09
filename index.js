433259acfc89a0f919268ea2bcdc15aecd228d19
phinic925 <phinicnyangate480@gmail.com>
2022-06-09 16:24:21 +0100

    commit tree ebbd811d816b5ce264429a8869868d4020ba4988

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName ="bob";
+console.log(customerName);
+function upperCaseCustomerName(){
+    customerName = (customerName.toUpperCase());
+
+}
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "phinic";
+
+}
